bc98af8ad68f7343e780ae4b63e338ae19f104b9
Movinzen <movin.son25@gmail.com>
2022-11-07 15:34:57 +0530

    user r commit

--- src/test/java/Pack1/Class1.java
+++ src/test/java/Pack1/Class1.java
@@ -10,22 +10,13 @@ public class Class1 {
 	public  void A3() {
 		System.out.println("A3");
 	}
-	public void B1() {
-		System.out.println("B1");
-	}
-	public  void B2() {
-		System.out.println("B2");
-	}
-	public  void B3() {
-		System.out.println("B3");
-	}
 	public void C1() {
 		System.out.println("C1");
 	}
-	public  void C2() {
+	public void C2() {
 		System.out.println("C2");
 	}
-	public  void C3() {
+	public void C3() {
 		System.out.println("C3");
 	}
 }
