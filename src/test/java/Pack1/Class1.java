ed91529e2e03de53895f6e14a45bc2df079cf848
Movinzen <movin.son25@gmail.com>
2022-11-07 15:16:00 +0530

    3rd set of methods C

--- src/test/java/Pack1/Class1.java
+++ src/test/java/Pack1/Class1.java
@@ -10,4 +10,13 @@ public class Class1 {
 	public  void A3() {
 		System.out.println("A3");
 	}
+	public void C1() {
+		System.out.println("C1");
+	}
+	public  void C2() {
+		System.out.println("C2");
+	}
+	public  void C3() {
+		System.out.println("C3");
+	}
 }
