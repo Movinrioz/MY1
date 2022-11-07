67a807f428b17eb2bfd25f43cd04abfdaf636a4a
Movinzen <movin.son25@gmail.com>
2022-11-07 13:07:23 +0530

    first 3 m code

---
+++ src/test/java/Pack1/Class1.java
@@ -0,0 +1,13 @@
+package Pack1;
+
+public class Class1 {
+	public void A1() {
+		System.out.println("A1");
+	}
+	public  void A2() {
+		System.out.println("A2");
+	}
+	public  void A3() {
+		System.out.println("A3");
+	}
+}
